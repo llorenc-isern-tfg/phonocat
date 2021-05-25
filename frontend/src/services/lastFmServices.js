7402ae6978e0177cf02681ad89ee59b4f048b4dc
Llorenç Isern Martín <lisernm@uoc.edu>
2021-05-25 02:40:26 +0200

    heroku

--- frontend/src/services/lastFmServices.js
+++ frontend/src/services/lastFmServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const searchAlbumsService = (albumTitle) => {
-    console.log(process.env.REACT_APP_LASTFM_URL)
+    alert(process.env.REACT_APP_LASTFM_URL)
     return axios.get(process.env.REACT_APP_LASTFM_URL, {
         params: {
             api_key: process.env.REACT_APP_LASTFM_KEY,
